f7eee44f4e97ac15553abc7820ad6842e8033fe6
s.murakami <smurakami.84@gmail.com>
2016-03-08 17:39:18 +0900

    initial commit

---
+++ denhack.js
@@ -0,0 +1,15 @@
+$('.arrow_question').each(function(i){
+  $(this).html('<input></input>');
+});
+$('input, button, textarea, select').css('-webkit-appearance', 'none');
+$('input, button, textarea, select').css('-moz-appearance', 'none');
+$('input, button, textarea, select').css('appearance', 'none');
+$('input, button, textarea, select').css('margin', '0');
+$('input, button, textarea, select').css('padding', '0');
+$('input, button, textarea, select').css('background', 'none');
+$('input, button, textarea, select').css('border', 'none');
+$('input, button, textarea, select').css('border-radius', '0');
+$('input, button, textarea, select').css('outline', 'none');
+$('input, button, textarea, select').css('font-family', 'inherit');
+$('input, button, textarea, select').css('font-size', 'inherit');
+
